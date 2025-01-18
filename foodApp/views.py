d1a888b8bee3d134b51fb109169b1fd6d7afd2c0
mahruf60 <mahruf9060@gmail.com>
2025-01-18 19:33:48 +0600

    Creating A E commerce FoodApp

---
+++ foodApp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import (
+    Item,
+)
+# Create your views here.
+
+def index(request):
+    item_list = Item.objects.all()
+    context = {
+        'item_list': item_list,
+    }
+    return render(request,'foodApp/index.html',context)
+
+def item(request):
+    return HttpResponse('<h1> This is an item view. </h1>')
+
+def detail(request,item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item':item,
+    }
+    return render(request,'foodApp/detail.html',context)
