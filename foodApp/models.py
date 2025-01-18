42062b2008883f2c7e2b8b55b274924f93528f25
mahruf60 <mahruf9060@gmail.com>
2025-01-18 23:01:23 +0600

    Static Files & Site Design

--- foodApp/models.py
+++ foodApp/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default = "https://www.thefuzzyduck.co.uk/wp-content/uploads/2024/05/image-coming-soon-placeholder-01-660x660.png")
     def __str__(self):
         return self.item_name 
     
