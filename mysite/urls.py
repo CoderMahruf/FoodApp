c9e627e3b786ba4831af7e9405a5d9a623309187
mahruf60 <mahruf9060@gmail.com>
2025-01-20 15:33:21 +0600

    Authentication In Django

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
+from users import views as user_views 
+from django.contrib.auth import views as authentication_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/',include('foodApp.urls')),
+    path('register/',user_views.register,name='register'),
+    path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('profile/',user_views.profilepage,name='profile'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
