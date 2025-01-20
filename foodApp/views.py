281bf3111251dc7acf19d850cd9dde3f363f451a
mahruf60 <mahruf9060@gmail.com>
2025-01-20 22:26:54 +0600

    Django Signals & Class Based Views

--- foodApp/views.py
+++ foodApp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from .forms import ItemForm
 from django.http import HttpResponse
+from django.views.generic.list import ListView 
+from django.views.generic.detail import DetailView 
+from django.views.generic.edit import CreateView 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import (
     Item,
 )
@@ -13,6 +17,11 @@ def index(request):
     }
     return render(request,'foodApp/index.html',context)
 
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'foodApp/index.html'
+    context_object_name = 'item_list'
+
 def item(request):
     return HttpResponse('<h1> This is an item view. </h1>')
 
@@ -23,12 +32,27 @@ def detail(request,item_id):
     }
     return render(request,'foodApp/detail.html',context)
 
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'foodApp/detail.html'
+
 def create_item(request):
     form = ItemForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect ('foodApp:index')
     return render(request,'foodApp/item-form.html',{'form':form})
+
+# This is a class based view for create item 
+
+class CreateItem(LoginRequiredMixin, CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'foodApp/item-form.html'
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
+
 
 def update_item(request,id):
     item = Item.objects.get(id=id)
